5edc1a8c23a30250408a202c32ef56d5871f0391
DallasLaComb <dallaslacombdrive@gmail.com>
2026-03-09 03:30:36 -0400

    feat: update API base URL in QA environment configuration

--- frontend/src/environments/environment.qa.ts
+++ frontend/src/environments/environment.qa.ts
@@ -9,6 +9,6 @@ export const environment = {
     domain: 'https://us-east-1rolgeorak.auth.us-east-1.amazoncognito.com',
   },
   api: {
-    baseUrl: '__API_BASE_URL__', // Replaced by pipeline with deployed API Gateway URL
+    baseUrl: 'https://9tbhgs85yk.execute-api.us-east-1.amazonaws.com, // Replaced by pipeline with deployed API Gateway URL
   },
 };
